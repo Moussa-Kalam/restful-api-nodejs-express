1aa4c6fc3d63cf6fba5f8c75e92b3567bfbf84c9
Moussa-Kalam <m.amzat@alustudent.com>
2023-03-16 23:12:12 +0200

    feat: Basic routing endpoints

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import routes from './src/routes/crmRoutes';
 
 const app = express();
 const PORT = 3000;
+
+routes(app);
 
 app.get('/', (req, res) => {
     res.send(`Node and express server is running on port ${PORT}`)
---
+++ src/routes/crmRoutes.js
@@ -0,0 +1,21 @@
+const routes = (app) => {
+    app.route('/contact')
+        .get((req, res) => 
+            res.send('GET request successful!')
+        )
+        .post((req, res) =>
+            res.send('POST request successful!')
+        )
+
+
+    app.route('/contact/:contactId')
+        .put((req, res) => 
+            res.send('PUT request successful!')
+        )
+
+        .delete((req, res) => 
+            res.send('DELETE request successful!')
+        )
+}
+
+export default routes;
